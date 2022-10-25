6f31089b3ab2738488f7a679232cb193aa0289ab
nevenaatic <74498373+nevenaatic@users.noreply.github.com>
2022-10-25 18:11:06 +0200

    feat: removed files

--- tennis-scheduler-web-app/src/components/TennisCourts.jsx
+++ tennis-scheduler-web-app/src/components/TennisCourts.jsx
@@ -1,21 +1,8 @@
-import React, { useState } from "react"
-import { useEffect } from "react";
-import { getTennisCourts } from "../api/TennisCourtApi";
+import React from "react"
+
 
 export const TennisCourts = () => {
 
-    const [tennisCourts, setTennisCourts] = useState([]);
-
-    useEffect(() => {
-        async function getCourts(){
-            getTennisCourts().then((data) => {
-                setTennisCourts(data.data);
-               console.log(data.data)
-            });
-        }
-        getCourts();
-         console.log(tennisCourts);
-    }, [])
     return (
         <> </>
     )
