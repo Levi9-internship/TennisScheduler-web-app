69228625a6c75d51af19b78d860c283f475cdc6a
Milica <sammilica99@gmail.com>
2022-10-27 23:58:08 +0200

    feat: unable to reserve timeslot for unauthorized user

--- tennis-scheduler-web-app/src/components/TennisCourtInfo.jsx
+++ tennis-scheduler-web-app/src/components/TennisCourtInfo.jsx
@@ -72,7 +72,7 @@ export const TennisCourtInfo = ({ id, image, name, surfaceType, description }) =
           <p> {description} </p>
           <p> {surfaceType} </p>
         </div>
-        <button className="addTimeslotBtn" onClick={add}>{buttonName}</button>
+        { (admin && tennisPlayer) ? <button className="addTimeslotBtn" onClick={add}>{buttonName}</button> : ""}
         { admin ? <span><Link to={`/court/${id}`}>
           <button className="addTimeslotBtn" >Change</button>
         </Link>
