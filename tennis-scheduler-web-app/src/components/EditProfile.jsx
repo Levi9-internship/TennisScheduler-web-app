ef8de3a57389796284160df0801c5c1fbc13e163
Danilo Rapaic <93617683+rapaicd@users.noreply.github.com>
2023-01-05 17:25:52 +0100

    feat: fixed editProfile

--- tennis-scheduler-web-app/src/components/EditProfile.jsx
+++ tennis-scheduler-web-app/src/components/EditProfile.jsx
@@ -8,9 +8,11 @@ import 'react-toastify/dist/ReactToastify.css';
 
 export const EditProfile = () => {
     const [user, setUser] = useState({ id: 0, firstName: "", lastName: "", email: "", phoneNumber: "", birthday: undefined, gender: undefined, address: { id: 0, street: "", city: "", number: undefined, country: "" } });
-    const [formErrors, setFormErrors] = useState({});
-    const [errorMessage, setErrorMessage] = useState("")
+
+    const [errorName, setErrorName] = useState("")
+    const [errorSurname, setErrorSurname] = useState("")
     const [isFormEmpty, setIsFormEmpty] = useState(true)
+    const [errorMessage,setErrorMessage]=useState("");
 
     useEffect(() => {
         getLoggedUser().then((response) => setUser(response.data)).catch(() => setErrorMessage("Something went wrong, try again."))
@@ -23,30 +25,22 @@ export const EditProfile = () => {
 
     const editProfileSubmit = e => {
         e.preventDefault();
-        setFormErrors(validation(user));
-        if (Object.keys(formErrors).length === 0 && !isFormEmpty) {
-            changeProfileInformation(user).then((response) => {
-                setUser(response.data);
-                toast.success("You successfully changed your data!", { position: toast.POSITION.BOTTOM_CENTER })
-            }).catch(() => toast.error("Failed to change information. Try again", { position: toast.POSITION.BOTTOM_CENTER }))
-        }
-    };
+        
+        setErrorSurname("")
+        setErrorName("")
 
-    const validation = form => {
-        const errors = {};
-        const phoneRegex = new RegExp(/^$|^([+]?[\s0-9]+)?(\d{3}|[(]?[0-9]+[)])?([-]?[\s]?[0-9])+$/i);
-        const lettersRegex = new RegExp(/^$|^[A-Za-z ]+$/);
-        const streetNumberRegex = new RegExp("^\\d+$");
+        if (user.firstName === "")
+            setErrorName("name isnt valid");
+        if (user.lastName === "") 
+            setErrorSurname("surname isnt valid");
+        
+        if(user.firstName === "" || user.lastName === "")
+            return
 
-        if (!form.firstName) errors.name = "Name is required!";
-        if (!form.lastName) errors.surname = "Surname is required!";
-        if (!phoneRegex.test(form.phoneNumber)) errors.phoneNumber = "Please enter valid format for phone number!"
-        if (!lettersRegex.test(form.address.country)) errors.country = "Only letters are valid!"
-        if (!lettersRegex.test(form.address.city)) errors.city = "Only letters are valid!"
-        if (!lettersRegex.test(form.address.street)) errors.street = "Only letters are valid!"
-        if (!streetNumberRegex.test(form.address.number)) errors.number = "Only numbers are allowed!"
-        setIsFormEmpty(false);
-        return errors;
+        changeProfileInformation(user).then((response) => {
+            setUser(response.data);
+            toast.success("You successfully changed your data!", { position: toast.POSITION.BOTTOM_CENTER })
+        }).catch(() => toast.error("Failed to change information. Try again", { position: toast.POSITION.BOTTOM_CENTER }))
     };
 
     return (
@@ -64,13 +58,12 @@ export const EditProfile = () => {
                             <form onSubmit={editProfileSubmit}>
                                 <label htmlFor="name"> Name</label>
                                 <input type="text" value={user.firstName} onChange={handleChange} className="form-control" id="name" name="firstName" />
-                                <p className="errors">{formErrors.name}</p>
+                                <p className="errors">{errorName}</p>
                                 <label htmlFor="surname"> Surname</label>
                                 <input type="text" value={user.lastName} onChange={handleChange} className="form-control" id="surname" name="lastName" />
-                                <p className="errors">{formErrors.surname}</p>
+                                <p className="errors">{errorSurname}</p>
                                 <label htmlFor="phone">Phone number</label>
                                 <input type="number" value={user.phoneNumber} onChange={handleChange} className="form-control" id="phone" name="phoneNumber" />
-                                <p className="errors">{formErrors.phoneNumber} </p>
                                 <label htmlFor="gender">Gender</label>
                                 <div className="row ">
                                     <div className="col">
@@ -86,24 +79,20 @@ export const EditProfile = () => {
                                     <div className="col">
                                         <label>Street</label>
                                         <input type="text" className="form-control" defaultValue={user.address.street} onChange={(e) => user.address.street = e.target.value} />
-                                        <p className="errors"> {formErrors.street}</p>
                                     </div>
                                     <div className="col">
                                         <label>Street number</label>
                                         <input type="text" className="form-control" defaultValue={user.address.number} onChange={(e) => user.address.number = e.target.value} />
-                                        <p className="errors"> {formErrors.number}</p>
                                     </div>
                                 </div>
                                 <div className="row">
                                     <div className="col">
                                         <label>City</label>
                                         <input type="text" className="form-control" defaultValue={user.address.city} onChange={(e) => user.address.city = e.target.value} />
-                                        <p className="errors"> {formErrors.city}</p>
                                     </div>
                                     <div className="col">
                                         <label>Country</label>
                                         <input type="text" className="form-control" defaultValue={user.address.country} onChange={(e) => user.address.country = e.target.value} />
-                                        <p className="errors"> {formErrors.country}</p>
                                     </div>
                                 </div>
                             </form>
